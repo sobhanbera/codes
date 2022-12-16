36472b394818168c38581ed0a22f6930e3af2c62
sobhanbera <sobhanbera258@gmail.com>
2022-12-16 18:44:32 +0530

    added or modified java files

--- Stacks.java
+++ Stacks.java
@@ -3,19 +3,20 @@ package Data_Structure;
 public class Stacks<E> {
 
 	private LinkedLists<E> ll = new LinkedLists<>();
-	
+
 	void push(E e) {
 		ll.add(e);
 	}
-	
+
 	E pop() throws Exception{
 		if(ll.isEmpty()) {
 			throw new Exception("Empty stack cannot be popped");
 		}
 		return ll.removeLast();
 	}
-	
+
 	E peek() throws Exception{
 		return ll.getLast();
 	}
 }
+
